7fc0c113d9cf54b3bcc8c83da3d0f1f952a46b4f
tvuleta8 <110557486+tvuleta8@users.noreply.github.com>
2022-08-20 17:56:40 +0200

    worked on search with api

--- src/Current.js
+++ src/Current.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Current.css";
+import "./Search.js";
 
 export default function Current() {
   return (
@@ -7,7 +8,11 @@ export default function Current() {
       <div className="col-sm-6 now">
         <h1 className="selectedCity">Paris</h1>
         <h1>
-          <img src="" alt="Clear" id="icon" />
+          <img
+            src="http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png"
+            alt="Clear"
+            id="icon"
+          />
         </h1>
         <h1 className="degrees">12°C</h1>
         <h6 className="units">
--- src/Search.js
+++ src/Search.js
@@ -1,15 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Search.css";
+import axios from "axios";
 
 export default function Search() {
+  const [city, setCity] = useState("");
+  const [loaded, setLoaded] = useState(false);
+  const [weather, setWeather] = useState({});
+
+  function displayWeather(response) {
+    setWeather({
+      temperature: response.data.main.temp,
+      wind: response.data.wind.speed,
+      humidity: response.data.main.humidity,
+      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+      description: response.data.weather[0].description,
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    let apiKey = "af1d49c44d7d38c5f24b52af719be555";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayWeather);
+  }
+
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+
+  let form = (
+    <form onSubmit={handleSubmit}>
+      <input type="search" placeholder="Enter a city.." onChange={updateCity} />
+      <button type="Submit">Search</button>
+    </form>
+  );
+
   return (
     <div className="Search">
-      <form>
+      <form onSubmit={handleSubmit}>
         <input
           type="text"
           name="Enter a city"
           className="city-search"
           placeholder="Enter a city"
+          onChange={updateCity}
         />
         <input type="submit" value="Search" className="search-button" />
         <input
