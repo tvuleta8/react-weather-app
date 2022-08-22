bd42ae79c22b5901ed0d6901845d718e2d16eb55
tvuleta8 <110557486+tvuleta8@users.noreply.github.com>
2022-08-22 12:13:43 +0200

    worked on forecast

--- src/Daily.js
+++ src/Daily.js
@@ -1,8 +1,15 @@
 import React from "react";
 import WeatherIcon from "./WeatherIcon";
 import "./Daily.css";
+import axios from "axios";
 
-export default function Daily() {
+export default function Daily(props) {
+  function getForecast(coordinates) {
+    let apiKey = "af1d49c44d7d38c5f24b52af719be555";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${props.coordinates.lat}&lon=${props.coordinates.lon}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
   return (
     <div className="Daily">
       <div className="row">
--- src/Weather.js
+++ src/Weather.js
@@ -11,6 +11,7 @@ export default function Weather(props) {
   function handleResponse(response) {
     setWeatherData({
       ready: true,
+      coordinates: response.data.coord,
       city: response.data.name,
       date: new Date(response.data.dt * 1000),
       temperature: response.data.main.temp,
@@ -60,7 +61,7 @@ export default function Weather(props) {
         <Current info={weatherData} />
         <hr />
         <h1 className="weekForecastHeading">This Week</h1>
-        <Daily />
+        <Daily coordinates={weatherData.coordinates} />
       </div>
     );
   } else {
